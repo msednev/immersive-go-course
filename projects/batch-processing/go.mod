8d206c2e3dcafd4ecbbfe8ffa09655d9529e3cbe
Maksim Sednev <msednev@protonmail.com>
2023-12-05 17:56:52 +0100

    WIP: implement download and conversion locally

--- projects/batch-processing/go.mod
+++ projects/batch-processing/go.mod
@@ -2,6 +2,9 @@ module github.com/CodeYourFuture/immersive-go-course/batch-processing
 
 go 1.19
 
+require github.com/aws/aws-sdk-go v1.48.12
+
 require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	gopkg.in/gographics/imagick.v2 v2.6.2 // indirect
 )
--- projects/batch-processing/main.go
+++ projects/batch-processing/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"encoding/csv"
 	"flag"
-	"log"
-	"os"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
 
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"gopkg.in/gographics/imagick.v2/imagick"
 )
 
@@ -26,12 +31,13 @@ func (c *Converter) Grayscale(inputFilepath string, outputFilepath string) error
 	return err
 }
 
-func ParseCsv(r io.Reader) ([]string) {
+// Parse provided csv and return an array of urls
+func ParseCsv(r io.Reader) []string {
 
 	reader := csv.NewReader(r)
 	reader.FieldsPerRecord = 1
 	result := make([]string, 0)
-	for i := 0;;i++ {
+	for i := 0; ; i++ {
 		record, err := reader.Read()
 		if err == io.EOF {
 			break
@@ -61,7 +67,7 @@ func DownloadImage(url string, dst io.Writer) (written int64) {
 	}
 
 	contentType := resp.Header.Get("content-type")
-	if contentType != "image/jpg" {
+	if contentType != "image/jpeg" {
 		log.Fatalf("Bad content type %v", contentType)
 	}
 
@@ -70,28 +76,59 @@ func DownloadImage(url string, dst io.Writer) (written int64) {
 		log.Fatal(err)
 	}
 	return written
-} 
+}
+
+func UploadImage(bucket *string, filename *string) error {
+	file, err := os.Open(*filename)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	sess, err := session.NewSession()
+	if err != nil {
+		return err
+	}
+	uploader := s3manager.NewUploader(sess)
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: bucket,
+		Key:    filename,
+		Body:   file,
+	})
+	return nil
+}
 
 func main() {
-	// Accept --input and --output arguments for the images
-	inputFilepath := flag.String("input", "", "A path to an csv with a list of files to be processed")
-	outputFilepath := flag.String("output", "", "A path to where the csv with results should be written")
+	// Accept --input and --output arguments for the csv files
+	inputCsv := flag.String("input", "", "A path to an csv with a list of files to be processed")
+	outputCsv := flag.String("output", "", "A path to where the csv with results should be written")
 	flag.Parse()
 
 	// Ensure that both flags were set
-	if *inputFilepath == "" || *outputFilepath == "" {
+	if *inputCsv == "" || *outputCsv == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
-
-	// inputfiles := ParseCsv(csv)
+	file, err := os.Open(*inputCsv)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	inputUrls := ParseCsv(file)
+	inputFiles := make([]string, len(inputUrls))
+	for i, url := range inputUrls {
+		filename := filepath.Join("inputs", strconv.Itoa(i) + ".jpg")
+		file, err := os.Create(filename)
+		if err != nil {
+			log.Println(err)
+		}
+		DownloadImage(url, file)
+		inputFiles[i] = filename
+		file.Close()
+	}
 
 	// Set up imagemagick
 	imagick.Initialize()
 	defer imagick.Terminate()
-
-	// Log what we're going to do
-	log.Printf("processing: %q to %q\n", *inputFilepath, *outputFilepath)
 
 	// Build a Converter struct that will use imagick
 	c := &Converter{
@@ -99,11 +136,19 @@ func main() {
 	}
 
 	// Do the conversion!
-	err := c.Grayscale(*inputFilepath, *outputFilepath)
-	if err != nil {
-		log.Printf("error: %v\n", err)
+	fmt.Println(inputFiles)
+	for _, inputFile := range inputFiles {
+		// Log what we're going to do
+		inputFileNameExt := filepath.Ext(inputFile)
+		inputFileNameBase := filepath.Base(inputFile)
+		inputFileWithoutExt := inputFileNameBase[:len(inputFileNameBase) - len(inputFileNameExt)]
+		outputFile := filepath.Join("outputs", inputFileWithoutExt + "_bw" + inputFileNameExt)
+		log.Printf("processing: %q to %q\n", inputFile, outputFile)
+		err = c.Grayscale(inputFile, outputFile)
+		if err != nil {
+			log.Printf("error: %v\n", err)
+		}
+		// Log what we did
+		log.Printf("processed: %q to %q\n", inputFile, outputFile)
 	}
-
-	// Log what we did
-	log.Printf("processed: %q to %q\n", *inputFilepath, *outputFilepath)
 }
