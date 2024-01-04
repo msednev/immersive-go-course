122a81c98d6d5398a50e40afd04515a06873c0c3
Maksim Sednev <msednev@protonmail.com>
2024-01-04 15:32:37 +0100

    implement basic auth

--- projects/http-auth/main.go
+++ projects/http-auth/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"fmt"
 	"html"
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -38,6 +41,28 @@ func main() {
 	http.HandleFunc("/500", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(500)
 		w.Write([]byte("500 Internal Server Error"))
+	})
+
+	http.HandleFunc("/authenticated", func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if ok {
+			usernameHash := sha256.Sum256([]byte(username))
+			passwordHash := sha256.Sum256([]byte(password))
+			expectedUsernameHash := sha256.Sum256([]byte(os.Getenv("AUTH_USERNAME")))
+			expectedPasswordHash := sha256.Sum256([]byte(os.Getenv("AUTH_PASSWORD")))
+
+			usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
+			passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+
+			if usernameMatch && passwordMatch {
+				w.Header().Add("Content-Type", "text/html")
+				w.Write([]byte("<!DOCTYPE html><html>Hello username!</html>"))
+				return
+			}
+		}
+
+		w.Header().Set("WWW-Authenticate", `Basic realm="localhost", charset="UTF-8"`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 	})
 
 	http.ListenAndServe(":8080", nil)
