75c1e20c007fb12b824600a3426db963d4180ae0
Maksim Sednev <msednev@protonmail.com>
2024-01-05 16:10:15 +0100

    WIP: setup db

--- projects/server-database/go.mod
+++ projects/server-database/go.mod
@@ -1,6 +1,8 @@
 module server-database
 
-go 1.21.4
+go 1.18
+
+require github.com/jackc/pgx/v4 v4.18.1
 
 require (
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
@@ -10,7 +12,6 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
-	github.com/jackc/pgx/v4 v4.18.1 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
--- projects/server-database/main.go
+++ projects/server-database/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
+
+	"github.com/jackc/pgx/v4"
 )
 
 type Image struct {
@@ -13,20 +18,55 @@ type Image struct {
 	URL     string `json:"url"`
 }
 
-var images = []Image{
-	{
-		Title: "Sunset",
-		AltText: "Clouds at sunset",
-		URL: "https://images.unsplash.com/photo-1506815444479-bfdb1e96c566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-	},
-	{
-		Title: "Mountain",
-		AltText: "A mountain at sunset",
-		URL: "https://images.unsplash.com/photo-1540979388789-6cee28a1cdc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-	},
-}
+// var images = []Image{
+// 	{
+// 		Title: "Sunset",
+// 		AltText: "Clouds at sunset",
+// 		URL: "https://images.unsplash.com/photo-1506815444479-bfdb1e96c566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+// 	},
+// 	{
+// 		Title: "Mountain",
+// 		AltText: "A mountain at sunset",
+// 		URL: "https://images.unsplash.com/photo-1540979388789-6cee28a1cdc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+// 	},
+// }
 
 func main() {
+	connString := os.Getenv("DATABASE_URL")
+	if connString == "" {
+		fmt.Fprintln(os.Stderr, "Provide a database URL")
+		os.Exit(1)
+	}
+
+	conn, err := pgx.Connect(context.Background(), connString)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable connect to database: %v", err)
+		os.Exit(1)
+	}
+	defer func() {
+		if err := conn.Close(context.Background()); err != nil {
+			fmt.Fprintf(os.Stderr, "unable to close database connection: %v", err)
+			os.Exit(1)
+		}
+	}()
+
+	rows, err := conn.Query(context.Background(), "SELECT title, url, alt_text FROM public.images")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to execute query: %v", err)
+		os.Exit(1)
+	}
+
+	var images []Image
+	var title, url, altText string
+
+	for rows.Next() {
+		if err = rows.Scan(&title, &url, &altText); err != nil {
+			fmt.Fprintf(os.Stderr, "unable to extract columns: %v", err)
+			os.Exit(1)
+		}
+		images = append(images, Image{Title: title, URL: url, AltText: altText})
+	}
+
 	http.HandleFunc("/images.json", func(w http.ResponseWriter, r *http.Request) {
 		queryParams := r.URL.Query()
 		indent, err := strconv.Atoi(queryParams.Get("indent"))
