061cfb037d89acd84daa2fa652b14b17bc8fd314
Maksim Sednev <msednev@protonmail.com>
2024-01-08 16:14:03 +0100

    WIP: implement basic static server

--- projects/multiple-servers/cmd/static-server/main.go
+++ projects/multiple-servers/cmd/static-server/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"servers/static"
 )
 
@@ -10,4 +14,25 @@ func main() {
 	portPtr := flag.Int("port", 0, "a port this server will be running on")
 	flag.Parse()
 	static.Run(*pathPtr, *portPtr)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		html, err := os.Open("./assets/index.html")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to open file: %v", err)
+			os.Exit(1)
+		}
+		body, err := io.ReadAll(html)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to read content: %v", err)
+			os.Exit(1)
+		}
+		if _, err := w.Write(body); err != nil {
+			fmt.Fprintf(os.Stderr, "unable to write request body: %v", err)
+			os.Exit(1)
+		}
+	})
+
+	http.ListenAndServe(":8082", mux)
 }
