390791360c8ef842198a39984df4f57ba680f838
Maksim Sednev <msednev@protonmail.com>
2024-02-20 23:02:50 +0100

    WIP: implement cache-tester

--- projects/memcached-clusters/main.go
+++ projects/memcached-clusters/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"github.com/bradfitz/gomemcache/memcache"
 	"log"
+	"reflect"
 	"strconv"
 	"strings"
+	"sync"
 )
-
 
 func parseInts(s string) ([]int, error) {
 	var ints []int
@@ -20,16 +23,60 @@ func parseInts(s string) ([]int, error) {
 	return ints, nil
 }
 
-func main() {
-	var mcrouter int
-	var memcachedStr string
+func getMemcachedClients(server string, ports []int) []*memcache.Client {
+	mcClients := make([]*memcache.Client, len(ports))
+	for index, port := range ports {
+		mcClients[index] = memcache.New(fmt.Sprintf("%s:%d", server, port))
+	}
+	return mcClients
+}
 
-	flag.IntVar(&mcrouter, "mcrouter", 0, "mcrouter port")
-	flag.StringVar(&memcachedStr,"memcacheds", "", "comma-separated list of memcached ports")
-	memcacheds, err := parseInts(memcachedStr)
+func main() {
+	var mcrouterPort int
+	var memcachedPorts string
+
+	flag.IntVar(&mcrouterPort, "mcrouter", 0, "mcrouter port")
+	flag.StringVar(&memcachedPorts, "memcacheds", "", "comma-separated list of memcached ports")
+	flag.Parse()
+	memcachedPortsParsed, err := parseInts(memcachedPorts)
 	if err != nil {
-		log.Fatalf("failed to parse command line arguments: %v: %v", memcachedStr, err)
+		log.Fatalf("failed to parse command line arguments: %v: %v", memcachedPorts, err)
 	}
 
-	
+	mcrouterClient := memcache.New(fmt.Sprintf("127.0.0.1:%d", mcrouterPort))
+	memcachedClients := getMemcachedClients("127.0.0.1", memcachedPortsParsed)
+
+	const key = "mykey"
+	const value = "my data"
+
+	if err := mcrouterClient.Set(&memcache.Item{Key: key, Value: []byte(value)}); err == nil {
+		log.Fatalf("failed to set key: %v", err)
+	}
+
+	var wg sync.WaitGroup
+
+	values := make(chan []byte, len(memcachedClients))
+
+	for _, client := range memcachedClients {
+		wg.Add(1)
+		go func(client *memcache.Client) {
+			defer wg.Done()
+			item, err := client.Get(key)
+			if err != nil {
+				log.Printf("cannot retrieve item: %v", err)
+			}
+			values <- item.Value
+		}(client)
+	}
+	wg.Wait()
+	close(values)
+
+	for el := range values {
+		if !reflect.DeepEqual([]byte(value), el) {
+			fmt.Println("the caches operate in sharded mode")
+			return
+		}
+	}
+
+	fmt.Println("the caches operate in replicated mode")
 }
